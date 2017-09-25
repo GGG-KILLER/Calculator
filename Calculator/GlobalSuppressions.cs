1d4930b4997e72a888ea544d2f305b3323a2b109
GGG KILLER <gggkiller2@gmail.com>
2017-09-25 20:49:37 -0300

    Clean up solution

---
+++ Calculator/GlobalSuppressions.cs
@@ -0,0 +1,14 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project. Project-level
+// suppressions either have no target or are given a specific
+// target and scoped to a namespace, type, member, etc.
+
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage ( "Style", "CC0001:You should use 'var' whenever possible.", Justification = "Non-obvious type", Scope = "member", Target = "~M:Calculator.UI.Forms.MainForm.BtnEquals_Click(System.Object,System.EventArgs)" )]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage ( "Style", "CC0105:You should use 'var' whenever possible.", Justification = "<Pending>", Scope = "member", Target = "~M:Calculator.Core.Lexer.Process(System.Action{System.String})~System.Collections.Generic.IEnumerable{Calculator.Core.Tokens.Token}" )]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage ( "Style", "CC0001:You should use 'var' whenever possible.", Justification = "<Pending>", Scope = "member", Target = "~M:Calculator.Core.Parser.ParseExpression~Calculator.Core.Nodes.ASTNode" )]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage ( "Style", "CC0001:You should use 'var' whenever possible.", Justification = "<Pending>", Scope = "member", Target = "~M:Calculator.Core.Parser.Expect(Calculator.Core.Tokens.TokenType)~Calculator.Core.Tokens.Token" )]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage ( "Style", "CC0001:You should use 'var' whenever possible.", Justification = "<Pending>", Scope = "member", Target = "~M:Calculator.Core.Parser.Expect(Calculator.Core.Tokens.TokenType,Calculator.Core.Tokens.TokenType)~Calculator.Core.Tokens.Token" )]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage ( "Style", "CC0001:You should use 'var' whenever possible.", Justification = "<Pending>", Scope = "member", Target = "~M:Calculator.Core.Parser.Consume(Calculator.Core.Tokens.TokenType,Calculator.Core.Tokens.TokenType)~Calculator.Core.Tokens.Token" )]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage ( "Style", "CC0001:You should use 'var' whenever possible.", Justification = "<Pending>", Scope = "member", Target = "~M:Calculator.Core.Parser.ParseLiteral~Calculator.Core.Nodes.ASTNode" )]
+
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage ( "Style", "CC0001:You should use 'var' whenever possible.", Justification = "<Pending>", Scope = "member", Target = "~M:Calculator.Core.Parser.ParseLiteral~Calculator.Core.Nodes.ValueExpression" )]
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Calculator
-A C# calculator using parsers
+My first attempt at making a parser.
+
+The calculator accepts functions, mathematical constants and has operator precedence
+
+# Dependencies
+- [GUtils.Text](https://github.com/GGG-KILLER/GUtils.NET/tree/master/GUtils.Text)
