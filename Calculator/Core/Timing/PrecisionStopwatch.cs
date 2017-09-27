f3196d287059107ada58779d9cfb0126489f4f73
GGG KILLER <gggkiller2@gmail.com>
2017-09-27 12:42:10 -0300

    Renamed MicroStopwatch.cs to PrecisionStopwatch.cs

--- Calculator/Core/Timing/PrecisionStopwatch.cs
+++ Calculator/Core/Timing/PrecisionStopwatch.cs
@@ -6,10 +6,7 @@ namespace Calculator.Core.Timing
 	public class PrecisionStopwatch : Stopwatch
 	{
 		public const Double TicksPerMicrosecond = TimeSpan.TicksPerMillisecond / 1000D;
-		public const Double TicksPerNanosecond = TicksPerMicrosecond / 1000D;
 
 		public Double ElapsedMicroseconds => this.ElapsedTicks / TicksPerMicrosecond;
-
-		public Double ElapsedNanoseconds => this.ElapsedTicks / TicksPerNanosecond;
 	}
 }
