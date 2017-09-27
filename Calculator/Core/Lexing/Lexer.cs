3c9abac0f5fd1f7159143d6075b1d22608e380c5
GGG KILLER <gggkiller2@gmail.com>
2017-09-27 09:40:59 -0300

    Reworked unary operators lexing
    Made it so that when lexing it'll remove two UnaryOp<->'s as they cancel out each other and also ignores any UnaryOp<+> as they don't contribute to anything.

--- Calculator/Core/Lexing/Lexer.cs
+++ Calculator/Core/Lexing/Lexer.cs
@@ -16,46 +16,52 @@ namespace Calculator.Core.Lexing
 			this._reader = new GUtils.Text.StringReader ( input );
 		}
 
-		public static IEnumerable<Token> Process ( String Input, Action<String> Log = null )
+		public static IEnumerable<Token> Process ( String Input )
 		{
 			return new Lexer ( Input )
-				.Process ( Log );
+				.Process ( );
 		}
 
-		private IEnumerable<Token> Process ( Action<String> Log )
+		private IEnumerable<Token> Process ( )
 		{
 			this.Tokens = new List<Token> ( );
 
 			Char ch = this._reader.Peek ( );
 			do
 			{
-				Log?.Invoke ( $"In char {ch}" );
 				if ( Char.IsDigit ( ch ) || ch == '.' )
 				{
-					Log?.Invoke ( "Reading number..." );
 					// Sets the new token as the last as we add it
 					// onto the list
-					this.Tokens.Add ( this.LastToken = GetNumber ( Log ) );
+					this.Tokens.Add ( this.LastToken = GetNumber ( ) );
 				}
 				else if ( Char.IsLetter ( ch ) )
 				{
-					Log?.Invoke ( "Reading identfier..." );
 					// Sets the new token as the last as we add it
 					// onto the list
 					this.Tokens.Add ( this.LastToken = GetIdentifier ( ) );
 				}
-				// + and - will always be operators unfortunately
-				//   as they need to work with unary operations
-				else if ( ch == '+' || ch == '-' )
+				// + and - will always be unary operators as they
+				//   need to work with unary operations
+				else if ( ( ch == '+' || ch == '-' ) && !this.LastToken.IsPossibleValue ( ) )
 				{
-					Log?.Invoke ( $"Reading operator {ch}..." );
 					var op = this._reader.ReadString ( 1 );
-					// Sets the new token as the last as we add it
-					// onto the list
-					this.Tokens.Add ( this.LastToken = new Token (
-						this.LastToken.IsPossibleValue ( ) ? TokenType.BinaryOp : TokenType.UnaryOp,
-						op.ToString ( )
-					) );
+					// We'll only consider - unary operators as +
+					// is virtually useless
+					if ( op == "-" )
+					{
+						// If last was a - too, then remove the
+						// last token and ignore current one and
+						// continue the lexing loop
+						if ( this.LastToken.Type == TokenType.UnaryOp && this.LastToken.Raw == "-" )
+						{
+							this.Tokens.RemoveAt ( this.Tokens.Count - 1 );
+							this.LastToken = this.Tokens[this.Tokens.Count - 1];
+							continue;
+						}
+						// Otherwise add the - to the token list
+						this.Tokens.Add ( this.LastToken = new Token ( TokenType.UnaryOp, op ) );
+					}
 				}
 				else if ( Language.IsOperator ( ch ) )
 				{
@@ -90,45 +96,28 @@ namespace Calculator.Core.Lexing
 			return this.Tokens;
 		}
 
-		//ident	::= letter, { letter | number } ;
+		//ident	 ::= letter, { letter | number } ;
 		private Token GetIdentifier ( )
-			=> new Token ( TokenType.Identifier, this._reader.ReadWhile ( Char.IsLetterOrDigit ) );
+		{
+			return new Token ( TokenType.Identifier, this._reader.ReadWhile ( Char.IsLetterOrDigit ) );
+		}
 
-		//number	::= { digit }, [ ".",  { digit } ] | ".", { digit };
-		private Token GetNumber ( Action<String> Log )
+		//number ::= { digit }, [ ".",  { digit } ] | ".", { digit };
+		private Token GetNumber ( )
 		{
 			var num = new StringBuilder ( );
 
-			Log?.Invoke ( "Reading first part of number..." );
-			Log?.Invoke ( $"Peek ( ) == {this._reader.Peek ( )}" );
 			num.Append ( this._reader.ReadWhile ( Char.IsNumber ) );
 
 			if ( this._reader.Peek ( ) == '.' )
 			{
-				Log?.Invoke ( "Reading floating point part..." );
 				num.Append ( this._reader.Read ( ) );
-				Log?.Invoke ( "Read dot. Reading rest of the number..." );
-				Log?.Invoke ( $"Peek ( ) == {this._reader.Peek ( )}" );
-				num.Append ( this._reader.ReadWhile ( ch =>
-				{
-					Log?.Invoke ( $"Checking {ch} for number-ish" );
-					return Char.IsNumber ( ch );
-				} ) );
-				Log?.Invoke ( "Read number." );
+				num.Append ( this._reader.ReadWhile ( Char.IsNumber ) );
 			}
 
 			if ( num.ToString ( ) == "" || num.ToString ( ) == "." )
 				throw new Exception ( $"Unexpected \"{num}\"." );
-
-			Log?.Invoke ( $"Entire number: {num}" );
-			try
-			{
-				return new Token ( TokenType.Number, num.ToString ( ) );
-			}
-			finally
-			{
-				Log?.Invoke ( "Returned token." );
-			}
+			return new Token ( TokenType.Number, num.ToString ( ) );
 		}
 	}
 }
