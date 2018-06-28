4f53367601a314b37a67d804dda62d036accc4d4
GGG KILLER <gggkiller2@gmail.com>
2018-06-28 07:24:21 -0300

    Add exception types

---
+++ Calculator.Lib/Exceptions/CalculatorException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Calculator.Lib.Exceptions
+{
+    public class CalculatorException : Exception
+    {
+        public CalculatorException ( String message ) : base ( message )
+        {
+        }
+
+        public CalculatorException ( String message, Exception innerException ) : base ( message, innerException )
+        {
+        }
+    }
+}
---
+++ Calculator.Lib/Exceptions/CalculatorRuntimeException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculator.Lib.Exceptions
+{
+    public class CalculatorRuntimeException : CalculatorException
+    {
+        public CalculatorRuntimeException ( String message ) : base ( message )
+        {
+        }
+
+        public CalculatorRuntimeException ( String message, Exception innerException ) : base ( message, innerException )
+        {
+        }
+    }
+}
