f14dbf241d31ac4b64b1536147c3b03121705981
GGG KILLER <gggkiller2@gmail.com>
2018-06-28 07:20:16 -0300

    Add basic visitors

---
+++ Calculator.Lib/Visitors/CTreeExecutor.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Calculator.Lib.Abstractions;
+using Calculator.Lib.AST;
+using Calculator.Lib.Definitions;
+using Calculator.Lib.Exceptions;
+
+namespace Calculator.Lib.Visitors
+{
+    public class CNodeTreeExecutor : ICNodeTreeVisitor<Double>
+    {
+        private readonly CalculatorLang Language;
+
+        public CNodeTreeExecutor ( CalculatorLang lang )
+        {
+            this.Language = lang;
+        }
+
+        public Double Visit ( BinaryOperatorExpression binaryOperator )
+            => this.Language.HasBinaryOperator ( binaryOperator.Operator )
+                ? this.Language.GetBinaryOperator ( binaryOperator.Operator )
+                    .Action ( binaryOperator.LeftHandSide.Accept ( this ), binaryOperator.RightHandSide.Accept ( this ) )
+                : throw new CalculatorRuntimeException ( $"Binary operator {binaryOperator.Operator} does not exist in this language." );
+
+        public Double Visit ( ConstantExpression constant )
+            => this.Language.HasConstant ( constant.Identifier )
+                ? this.Language.GetConstant ( constant.Identifier ).Value
+                : throw new CalculatorRuntimeException ( $"Constant {constant.Identifier} does not exist in this language." );
+
+        public Double Visit ( FunctionCallExpression functionCall )
+            => this.Language.HasFunction ( functionCall.Identifier )
+                ? ( Double ) this.Language.GetFunction ( functionCall.Identifier )
+                    .DynamicInvoke ( Array.ConvertAll ( functionCall.Arguments, node => node.Accept ( this ) ) )
+                : throw new CalculatorRuntimeException ( $"Function {functionCall.Identifier} does not exist in this language." );
+
+        public Double Visit ( NumberExpression number )
+            => number.Value;
+
+        public Double Visit ( ParenthesisExpression parenthesis )
+            => parenthesis.InnerExpression.Accept ( this );
+
+        public Double Visit ( UnaryOperatorExpression unaryOperator )
+            => this.Language.HasUnaryOperator ( unaryOperator.Operator )
+                ? this.Language.GetUnaryOperator ( unaryOperator.Operator )
+                    .Action ( unaryOperator.Operand.Accept ( this ) )
+                : throw new CalculatorRuntimeException ( $"Unary operator {unaryOperator.Operator} does not exist in this language." );
+    }
+}
---
+++ Calculator.Lib/Visitors/MathExpressionReconstructor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Calculator.Lib.Abstractions;
+using Calculator.Lib.AST;
+
+namespace Calculator.Lib.Visitors
+{
+    public class MathExpressionReconstructor : ICNodeTreeVisitor<String>
+    {
+        public String Visit ( BinaryOperatorExpression binaryOperator )
+            => $"({binaryOperator.LeftHandSide.Accept ( this )} {binaryOperator.Operator} {binaryOperator.RightHandSide.Accept ( this )})";
+
+        public String Visit ( ConstantExpression constant )
+            => constant.Identifier;
+
+        public String Visit ( FunctionCallExpression functionCall )
+            => $"{functionCall.Identifier} ( {String.Join ( ", ", Array.ConvertAll ( functionCall.Arguments, node => node.Accept ( this ) ) )} )";
+
+        public String Visit ( NumberExpression number )
+            => number.Value.ToString ( );
+
+        public String Visit ( ParenthesisExpression parenthesis )
+            => $"({parenthesis.InnerExpression.Accept ( this )})";
+
+        public String Visit ( UnaryOperatorExpression unaryOperator )
+            => unaryOperator.Fix == Definitions.UnaryOperatorFix.Postfix
+                ? $"{unaryOperator.Operand.Accept ( this )}{unaryOperator.Operator}"
+                : $"{unaryOperator.Operator}{unaryOperator.Operand.Accept ( this )}";
+    }
+}
