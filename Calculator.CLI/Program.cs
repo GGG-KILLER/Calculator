58f200598a2fb43f005a3fba2350e9c55b1c92aa
GGG KILLER <gggkiller2@gmail.com>
2018-10-02 16:13:00 -0300

    Remove unused parts and fix cli printing

--- Calculator.CLI/Program.cs
+++ Calculator.CLI/Program.cs
@@ -1,11 +1,11 @@
 ﻿using System;
-using System.Diagnostics;
 using Calculator.Lib;
 using Calculator.Lib.AST;
 using Calculator.Lib.Definitions;
 using Calculator.Lib.Exceptions;
 using Calculator.Lib.Visitors;
 using GParse.Common.Errors;
+using GUtils.Timing;
 
 namespace Calculator.CLI
 {
@@ -14,92 +14,132 @@ namespace Calculator.CLI
         private static CalculatorLang Language;
         private static MathExpressionReconstructor ExpressionReconstructor;
         private static CNodeTreeExecutor TreeExecutor;
+        private static TimingArea Root;
 
         private static void Main ( )
         {
-            Language = BuildLanguage ( );
-            ExpressionReconstructor = new MathExpressionReconstructor ( );
-            TreeExecutor = new CNodeTreeExecutor ( Language );
+            using ( Root = new TimingArea ( "Initialization" ) )
+            {
+                TimingArea r = Root;
+                using ( Root = new TimingArea ( "Building the language", Root ) )
+                    Language = BuildLanguage ( );
+                Root = r;
+                using ( Root.TimeLine ( "Initializing the reconstructor" ) )
+                    ExpressionReconstructor = new MathExpressionReconstructor ( );
+                using ( Root.TimeLine ( "Initializing the executor" ) )
+                    TreeExecutor = new CNodeTreeExecutor ( Language );
+            }
 
             while ( true )
             {
+                Console.Write ( '>' );
                 var line = Console.ReadLine ( );
                 line = line.Trim ( );
-                if ( line == "exit" )
+                if ( line == "q" || line == "e" || line == "quit" || line == "exit" )
                     break;
 
                 if ( line.StartsWith ( "b " ) || line.StartsWith ( "bench " ) )
                     BenchmarkWithExpression ( line.Substring ( line.IndexOf ( ' ' ) ) );
                 else
                     ExecuteExpression ( line );
-
             }
         }
 
         private static CalculatorLang BuildLanguage ( )
         {
             var lang = new CalculatorLang ( "Math", new Version ( 1, 0, 0 ) );
+
             // Constants
-            lang.AddConstant ( "E", Math.E );
-            lang.AddConstant ( "e", Math.E );
-            lang.AddConstant ( "PI", Math.PI );
-            lang.AddConstant ( "Pi", Math.PI );
-            lang.AddConstant ( "pi", Math.PI );
-            lang.AddConstant ( "π", Math.PI );
+            using ( Root.TimeLine ( "Adding constants" ) )
+            {
+                lang.AddConstant ( "E", Math.E );
+                lang.AddConstant ( "e", Math.E );
+                lang.AddConstant ( "PI", Math.PI );
+                lang.AddConstant ( "Pi", Math.PI );
+                lang.AddConstant ( "pi", Math.PI );
+                lang.AddConstant ( "π", Math.PI );
+            }
 
             // Unary operators
-            lang.AddUnaryOperator ( UnaryOperatorFix.Prefix, "-", 1, n => -n );
-            lang.AddUnaryOperator ( UnaryOperatorFix.Prefix, "~", 1, n => ~( ( Int64 ) n ) );
-            lang.AddUnaryOperator ( UnaryOperatorFix.Postfix, "!", 1, n =>
+            using ( Root.TimeLine ( "Adding unary operators" ) )
             {
-                if ( Double.IsInfinity ( n ) )
-                    return n;
-                var r = 1D;
-                for ( var i = 2; i < n && !Double.IsInfinity ( r ) && !Double.IsInfinity ( i ); i++ )
-                    r *= i;
-                return r;
-            } );
+                lang.AddUnaryOperator ( UnaryOperatorFix.Prefix, "-", 1, n => -n );
+                lang.AddUnaryOperator ( UnaryOperatorFix.Prefix, "~", 1, n => ~( ( Int64 ) n ) );
+                lang.AddUnaryOperator ( UnaryOperatorFix.Postfix, "!", 1, n =>
+                {
+                    if ( Double.IsInfinity ( n ) )
+                        return n;
+                    var r = 1D;
+                    for ( var i = 2; i < n && !Double.IsInfinity ( r ) && !Double.IsInfinity ( i ); i++ )
+                        r *= i;
+                    return r;
+                } );
+            }
 
             // Binary operators Binary operators - Math operators
-            lang.AddBinaryOperator ( OperatorAssociativity.Left, "+", 1, ( lhs, rhs ) => lhs + rhs );
-            lang.AddBinaryOperator ( OperatorAssociativity.Left, "-", 1, ( lhs, rhs ) => lhs - rhs );
-            lang.AddBinaryOperator ( OperatorAssociativity.Left, "*", 2, ( lhs, rhs ) => lhs * rhs );
-            lang.AddBinaryOperator ( OperatorAssociativity.Left, "/", 2, ( lhs, rhs ) => lhs / rhs );
-            lang.AddBinaryOperator ( OperatorAssociativity.Left, "%", 2, ( lhs, rhs ) => lhs % rhs );
-            lang.AddBinaryOperator ( OperatorAssociativity.Right, "^", 3, ( lhs, rhs ) => Math.Pow ( lhs, rhs ) );
+            using ( Root.TimeLine ( "Adding math binary operators" ) )
+            {
+                lang.AddBinaryOperator ( OperatorAssociativity.Left, "+", 1, ( lhs, rhs ) => lhs + rhs );
+                lang.AddBinaryOperator ( OperatorAssociativity.Left, "-", 1, ( lhs, rhs ) => lhs - rhs );
+                lang.AddBinaryOperator ( OperatorAssociativity.Left, "*", 2, ( lhs, rhs ) => lhs * rhs );
+                lang.AddBinaryOperator ( OperatorAssociativity.Left, "/", 2, ( lhs, rhs ) => lhs / rhs );
+                lang.AddBinaryOperator ( OperatorAssociativity.Left, "%", 2, ( lhs, rhs ) => lhs % rhs );
+                lang.AddBinaryOperator ( OperatorAssociativity.Right, "^", 3, ( lhs, rhs ) => Math.Pow ( lhs, rhs ) );
+            }
+
             // Binary operators - Logical operators
-            lang.AddBinaryOperator ( OperatorAssociativity.Left, "<<", 4, ( lhs, rhs ) => ( Int64 ) lhs << ( Int32 ) rhs );
-            lang.AddBinaryOperator ( OperatorAssociativity.Left, ">>", 4, ( lhs, rhs ) => ( Int64 ) lhs >> ( Int32 ) rhs );
-            lang.AddBinaryOperator ( OperatorAssociativity.Left, "&", 5, ( lhs, rhs ) => ( Int64 ) lhs & ( Int64 ) rhs );
-            lang.AddBinaryOperator ( OperatorAssociativity.Left, "|", 5, ( lhs, rhs ) => ( Int64 ) lhs | ( Int64 ) rhs );
+            using ( Root.TimeLine ( "Adding logical binary operators" ) )
+            {
+                lang.AddBinaryOperator ( OperatorAssociativity.Left, "<<", 4, ( lhs, rhs ) => ( Int64 ) lhs << ( Int32 ) rhs );
+                lang.AddBinaryOperator ( OperatorAssociativity.Left, ">>", 4, ( lhs, rhs ) => ( Int64 ) lhs >> ( Int32 ) rhs );
+                lang.AddBinaryOperator ( OperatorAssociativity.Left, "&", 5, ( lhs, rhs ) => ( Int64 ) lhs & ( Int64 ) rhs );
+                lang.AddBinaryOperator ( OperatorAssociativity.Left, "|", 5, ( lhs, rhs ) => ( Int64 ) lhs | ( Int64 ) rhs );
+            }
 
             // Functions Functions - Math
-            lang.AddFunction ( "abs", Math.Abs );
-            lang.AddFunction ( "acos", Math.Acos );
-            lang.AddFunction ( "asin", Math.Asin );
-            lang.AddFunction ( "atan", Math.Atan );
-            lang.AddFunction ( "atan2", Math.Atan2 );
-            lang.AddFunction ( "ceil", Math.Ceiling );
-            lang.AddFunction ( "cos", Math.Cos );
-            lang.AddFunction ( "cosh", Math.Cosh );
-            lang.AddFunction ( "exp", Math.Exp );
-            lang.AddFunction ( "floor", Math.Floor );
-            lang.AddFunction ( "ln", ( Func<Double, Double> ) Math.Log );
-            lang.AddFunction ( "log", ( Func<Double, Double, Double> ) Math.Log );
-            lang.AddFunction ( "log10", Math.Log10 );
-            lang.AddFunction ( "log2", ( n ) => Math.Log ( n, 2 ) );
-            lang.AddFunction ( "max", Math.Max );
-            lang.AddFunction ( "min", Math.Min );
-            lang.AddFunction ( "pow", Math.Pow );
-            lang.AddFunction ( "round", Math.Round );
-            lang.AddFunction ( "sin", Math.Sin );
-            lang.AddFunction ( "sinh", Math.Sinh );
-            lang.AddFunction ( "sqrt", Math.Sqrt );
-            lang.AddFunction ( "tan", Math.Tan );
-            lang.AddFunction ( "tanh", Math.Tanh );
-            lang.AddFunction ( "truncate", Math.Truncate );
+            using ( Root.TimeLine ( "Adding math functions" ) )
+            {
+                lang.AddFunction ( "abs", Math.Abs );
+                lang.AddFunction ( "acos", Math.Acos );
+                lang.AddFunction ( "asin", Math.Asin );
+                lang.AddFunction ( "atan", Math.Atan );
+                lang.AddFunction ( "atan2", Math.Atan2 );
+                lang.AddFunction ( "ceil", Math.Ceiling );
+                lang.AddFunction ( "cos", Math.Cos );
+                lang.AddFunction ( "cosh", Math.Cosh );
+                lang.AddFunction ( "exp", Math.Exp );
+                lang.AddFunction ( "floor", Math.Floor );
+                lang.AddFunction ( "ln", ( Func<Double, Double> ) Math.Log );
+                lang.AddFunction ( "log", ( Func<Double, Double, Double> ) Math.Log );
+                lang.AddFunction ( "log10", Math.Log10 );
+                lang.AddFunction ( "log2", ( n ) => Math.Log ( n, 2 ) );
+                lang.AddFunction ( "max", Math.Max );
+                lang.AddFunction ( "min", Math.Min );
+                lang.AddFunction ( "pow", Math.Pow );
+                lang.AddFunction ( "round", Math.Round );
+                lang.AddFunction ( "sin", Math.Sin );
+                lang.AddFunction ( "sinh", Math.Sinh );
+                lang.AddFunction ( "sqrt", Math.Sqrt );
+                lang.AddFunction ( "tan", Math.Tan );
+                lang.AddFunction ( "tanh", Math.Tanh );
+                lang.AddFunction ( "truncate", Math.Truncate );
+                lang.AddFunction ( "rot", ( a, b, c ) => ( b * c ) / a );
+                /*
+                 * a - b
+                 * c - d
+                 *
+                 * a*d = c*b
+                 *
+                 * d = (c*b)/a
+                 */
+                lang.AddFunction ( "ruleOfThree", ( a, b, c ) => ( b * c ) / a );
+            }
+
             // Functions - Logical
-            lang.AddFunction ( "xor", ( lhs, rhs ) => ( Int64 ) lhs ^ ( Int64 ) rhs );
+            using ( Root.TimeLine ( "Adding logical functions" ) )
+            {
+                lang.AddFunction ( "xor", ( lhs, rhs ) => ( Int64 ) lhs ^ ( Int64 ) rhs );
+            }
 
             return lang;
         }
@@ -108,17 +148,27 @@ namespace Calculator.CLI
         {
             try
             {
-                var sw = Stopwatch.StartNew ( );
-                var lexer = new CalculatorLexer ( Language, expression );
-                var parser = new CalculatorParser ( Language, lexer );
-                CASTNode ast = parser.Parse ( );
-                sw.Stop ( );
-                Console.WriteLine ( $"{HumanTime ( sw.ElapsedTicks )} elapsed parsing." );
-                sw.Restart ( );
-                Double res = ast.Accept ( TreeExecutor );
-                sw.Stop ( );
-                Console.WriteLine ( $"{HumanTime ( sw.ElapsedTicks )} elapsed executing." );
-                Console.WriteLine ( $"{ast.Accept ( ExpressionReconstructor )} = {res}" );
+                using ( var exArea = new TimingArea ( $"Executing: {expression}" ) )
+                {
+                    CalculatorParser parser;
+                    CASTNode ast;
+                    Double res;
+                    String rec;
+
+                    using ( exArea.TimeLine ( "Lexing" ) )
+                        parser = new CalculatorParser ( Language, new CalculatorLexer ( Language, expression ) );
+
+                    using ( exArea.TimeLine ( "Parsing" ) )
+                        ast = parser.Parse ( );
+
+                    using ( exArea.TimeLine ( "Execution" ) )
+                        res = ast.Accept ( TreeExecutor );
+
+                    using ( exArea.TimeLine ( "Reconstruction" ) )
+                        rec = ast.Accept ( ExpressionReconstructor );
+
+                    exArea.Log ( $"{rec} = {res}" );
+                }
             }
             catch ( LexException ex )
             {
@@ -150,20 +200,6 @@ namespace Calculator.CLI
         {
             for ( var i = 0; i < 50; i++ )
                 ExecuteExpression ( expression );
-        }
-
-        private static readonly Double TicksPerMicrosecond = TimeSpan.TicksPerMillisecond / 1000D;
-
-        private static String HumanTime ( Int64 ticks )
-        {
-            if ( ticks > TimeSpan.TicksPerMinute )
-                return $"{( Double ) ticks / TimeSpan.TicksPerMinute}m";
-            else if ( ticks > TimeSpan.TicksPerSecond )
-                return $"{( Double ) ticks / TimeSpan.TicksPerSecond}s";
-            else if ( ticks > TimeSpan.TicksPerMillisecond )
-                return $"{( Double ) ticks / TimeSpan.TicksPerMillisecond}ms";
-            else
-                return $"{( Double ) ticks / TicksPerMicrosecond}μs";
         }
     }
 }
