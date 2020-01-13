76ae6da1f3de8a45f0abfcdf057f5782efc346f5
GGG KILLER <gggkiller2@gmail.com>
2020-01-12 21:54:11 -0300

    Implement IEquatable for SpecialBinaryOperator

--- Calculator/Definitions/SpecialBinaryOperator.cs
+++ Calculator/Definitions/SpecialBinaryOperator.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Calculator.Definitions
 {
@@ -6,7 +7,7 @@ namespace Calculator.Definitions
     /// Represents a binary operator that cannot be expressed with a string because of its behavior
     /// (like implicit multiplication or exponentiation by superscript)
     /// </summary>
-    public readonly struct SpecialBinaryOperator
+    public readonly struct SpecialBinaryOperator : IEquatable<SpecialBinaryOperator>
     {
         /// <summary>
         /// Initializes this <see cref="SpecialBinaryOperator"/> with the provided information
@@ -42,5 +43,46 @@ namespace Calculator.Definitions
         /// The body of the special operator
         /// </summary>
         public Func<Double, Double, Double> Body { get; }
+
+        #region Object
+
+        /// <inheritdoc/>
+        public override Boolean Equals ( Object obj ) => obj is SpecialBinaryOperator @operator && this.Equals ( @operator );
+
+        /// <inheritdoc/>
+        public override Int32 GetHashCode ( )
+        {
+            var hashCode = 634421626;
+            hashCode = hashCode * -1521134295 + this.Type.GetHashCode ( );
+            hashCode = hashCode * -1521134295 + this.Precedence.GetHashCode ( );
+            hashCode = hashCode * -1521134295 + this.Associativity.GetHashCode ( );
+            hashCode = hashCode * -1521134295 + EqualityComparer<Func<Double, Double, Double>>.Default.GetHashCode ( this.Body );
+            return hashCode;
+        }
+
+        #endregion Object
+
+        #region IEquatable<SpecialBinaryOperator>
+
+        /// <inheritdoc/>
+        public Boolean Equals ( SpecialBinaryOperator other ) => this.Type == other.Type && this.Precedence == other.Precedence && this.Associativity == other.Associativity && EqualityComparer<Func<Double, Double, Double>>.Default.Equals ( this.Body, other.Body );
+
+        #endregion IEquatable<SpecialBinaryOperator>
+
+        /// <summary>
+        /// Checks two special binary operators for equality
+        /// </summary>
+        /// <param name="left"></param>
+        /// <param name="right"></param>
+        /// <returns></returns>
+        public static Boolean operator == ( SpecialBinaryOperator left, SpecialBinaryOperator right ) => left.Equals ( right );
+
+        /// <summary>
+        /// Checks two special binary operators for inequality
+        /// </summary>
+        /// <param name="left"></param>
+        /// <param name="right"></param>
+        /// <returns></returns>
+        public static Boolean operator != ( SpecialBinaryOperator left, SpecialBinaryOperator right ) => !( left == right );
     }
 }
