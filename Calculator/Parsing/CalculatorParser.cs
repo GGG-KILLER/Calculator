1f2bd72d28159e5e8598f3c69f716a58946f7994
GGG KILLER <gggkiller2@gmail.com>
2021-06-18 19:53:24 -0300

    Update the parser to remove the position container and use Option<T> methods.

--- Calculator/Parsing/CalculatorParser.cs
+++ Calculator/Parsing/CalculatorParser.cs
@@ -1,5 +1,4 @@
 ﻿using Calculator.Lexing;
-using Calculator.Parsing.AST;
 using GParse;
 using GParse.Errors;
 using GParse.Lexing;
@@ -13,28 +12,21 @@ namespace Calculator.Parsing
     /// </summary>
     public class CalculatorParser : PrattParser<CalculatorTokenType, CalculatorTreeNode>
     {
-
         /// <summary>
         /// <inheritdoc />
         /// </summary>
-        /// <param name="positionContainer"></param>
         /// <param name="tokenReader"></param>
         /// <param name="prefixModules"></param>
         /// <param name="infixModules"></param>
         /// <param name="diagnostics"></param>
         protected internal CalculatorParser(
-            IPositionContainer positionContainer,
             ITokenReader<CalculatorTokenType> tokenReader,
             PrattParserModuleTree<CalculatorTokenType, IPrefixParselet<CalculatorTokenType, CalculatorTreeNode>> prefixModules,
             PrattParserModuleTree<CalculatorTokenType, IInfixParselet<CalculatorTokenType, CalculatorTreeNode>> infixModules,
             DiagnosticList diagnostics)
-            : base(tokenReader, prefixModules, infixModules, diagnostics) =>
-            PositionContainer = positionContainer;
-
-        /// <summary>
-        /// The position container to use when resolving diagnostic locations.
-        /// </summary>
-        public IPositionContainer PositionContainer { get; }
+            : base(tokenReader, prefixModules, infixModules, diagnostics)
+        {
+        }
 
         /// <summary>
         /// Parses an expression
@@ -42,11 +34,10 @@ namespace Calculator.Parsing
         /// <returns></returns>
         public CalculatorTreeNode Parse()
         {
-            if (!TryParseExpression(out var expr))
-                throw new FatalParsingException(PositionContainer.GetLocation(TokenReader.Lookahead().Range.Start), "Unable to parse this expression.");
+            var expr = ParseExpression().UnwrapOrElse(() => throw new FatalParsingException(TokenReader.Lookahead().Range, "Unable to parse this expression."));
 
             if (!TokenReader.Accept(CalculatorTokenType.EndOfExpression, out _))
-                Diagnostics.Report(CalculatorDiagnostics.SyntaxError.ThingExpected(PositionContainer.GetLocation(TokenReader.Lookahead().Range.Start), "EOF"));
+                Diagnostics.Report(CalculatorDiagnostics.SyntaxError.ThingExpected(TokenReader.Lookahead().Range, "EOF"));
 
             return expr;
         }
